c087dd5f53464fc835c3a60619c05afe430e036c
David Roper <ropdav@dnpws13>
2023-12-05 15:58:29 -0500

    can now create multiple sorted csv

--- sort-data.py
+++ sort-data.py
@@ -5,12 +5,14 @@ import glob
 current_dir = os.getcwd()
 
 os.chdir(current_dir)
-
+i = 0
 for file in glob.glob("*.csv"):
     print(file)
     df = pd.read_csv(file)
     #df.drop_duplicates(keep=False)
     df = df.drop(df[df['Schedule name'] == 'Schedule name'].index)
-    df.to_csv('testframe.csv', encoding='utf-8', index=False)
+    name = 'testframe'
+    name = name + str(i) + '.csv'
+    df.to_csv(name, encoding='utf-8', index=False)
 
 
