37f72edad5d9878d063bf66c0ca7e9b618b9d2ce
David Roper <ropdav@dnpws13>
2023-12-07 15:47:17 -0500

    can now convert and clean text files

--- sort-data.py
+++ sort-data.py
@@ -19,6 +19,16 @@ for file in glob.glob("*.csv"):
     path = current_dir + '/cleanData/'
     df.to_csv(path+name, encoding='utf-8', index=False)
     print(file + ' is now cleaned')
+
+for file in glob.glob("*.txt"):
+    df = pd.read_csv(file, sep= "\t")
+    #this code drops all title rows that arent the first one
+    df = df.drop(df[df[df.columns[0]] == df.columns[0]].index)
+
+    name = file[:-4] + '-clean' + '.csv'
+    path = current_dir + '/cleanData/'
+    df.to_csv(path+name, encoding='utf-8', index=False)
+    print(file + ' is now cleaned')
 print('\ncleaned data files can be found in the /clean-data folder, thanks!')
 
 
