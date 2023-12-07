fa8e73205db6c7434c252589ce8dbb9525e5490a
David Roper <ropdav@dnpws13>
2023-12-07 14:26:37 -0500

    file now sorts cleaned data into a folder

--- sort-data.py
+++ sort-data.py
@@ -5,14 +5,18 @@ import glob
 current_dir = os.getcwd()
 
 os.chdir(current_dir)
+
 i = 0
+if(not os.path.isdir('clean-data')):
+    os.mkdir(current_dir + '/clean-data/')
+
 for file in glob.glob("*.csv"):
     print(file)
     df = pd.read_csv(file)
     #df.drop_duplicates(keep=False)
     df = df.drop(df[df['Schedule name'] == 'Schedule name'].index)
-    name = 'testframe'
-    name = name + str(i) + '.csv'
-    df.to_csv(name, encoding='utf-8', index=False)
+    name = file + '-clean' + '.csv'
+    path = current_dir + '/clean-data/'
+    df.to_csv(path+name, encoding='utf-8', index=False)
 
 
